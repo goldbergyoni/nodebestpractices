8b0da5f373ecc9a6fb55a3ca3b5f23d75e33c483
Yoni Goldberg <me@goldbergyoni.com>
2017-11-22 12:39:46 +0200

    A change without real meaning rather for testing the CI state

--- services/order/orderAPI.js
+++ services/order/orderAPI.js
@@ -4,7 +4,6 @@ const express = require('express'),
     newOrderService = require('./orderService'),
     order = require('./order');
 
-
 router.post('/', async(req, res, next) => {
     console.log(`Order controller was called to add new Order ${util.inspect(req.body)}`);
     const newOrderToAdd = Object.assign(new order(), req.body);
