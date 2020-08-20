1fda1a9ec3d29e360a3fce62f2ab09869389ddd2
Kevyn Bruyere <kevynb@users.noreply.github.com>
2020-08-20 10:49:43 +0200

    Update Dockerfile and switch with express

--- sections/examples/dockerfile/src/app.ts
+++ sections/examples/dockerfile/src/app.ts
@@ -1,9 +1,9 @@
-import * as Koa from 'koa';
-const app = new Koa();
+import * as express from 'express';
+const app = express();
 
-app.use(async ctx => {
-    ctx.body = 'Hello World';
-});
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
 app.listen(3000, () => {
     console.log('Navigate to http://localhost:3000');
